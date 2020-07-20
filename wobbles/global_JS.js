ad80f6747830396797b4157c40e16a8ee9f4ce44
EceKayaMPI <61702784+EceKayaMPI@users.noreply.github.com>
2020-07-20 21:48:35 +0200

    so close..
    one last step: get participant index and pass it into the experiment & plugins etc.

---
+++ wobbles/global_JS.js
@@ -0,0 +1,52 @@
+
+var stims4all_demo = [
+    {
+      "participant_order": 1,
+      "stimlist": [
+      {stimulus: 'stimfiles/AM_8.00_Hz_0.18dep.wav'},
+      {stimulus: 'stimfiles/AM_8.00_Hz_0.18dep.wav'},
+      {stimulus: 'stimfiles/AM_8.00_Hz_0.18dep.wav'},
+      ]
+    },
+    {
+      "participant_order": 2,
+      "stimlist": [
+      {stimulus: 'stimfiles/noise1.wav'},
+      {stimulus: 'stimfiles/noise1.wav'},
+      {stimulus: 'stimfiles/AM_4.00_Hz_0.18dep.wav'},
+      ]
+    },
+    {
+      "participant_order": 3,
+      "stimlist": [
+      {stimulus: 'stimfiles/noise1.wav'},
+      {stimulus: 'stimfiles/noise1.wav'},
+      {stimulus: 'stimfiles/noise1.wav'},
+      ]
+    },
+    {
+      "participant_order": 4,
+      "stimlist": [
+      {stimulus: 'stimfiles/AM_8.00_Hz_0.18dep.wav'},
+      {stimulus: 'stimfiles/AM_8.00_Hz_0.18dep.wav'},
+      {stimulus: 'stimfiles/AM_8.00_Hz_0.18dep.wav'},
+      ]
+    },
+    {
+      "participant_order": 5,
+      "stimlist": [
+      {stimulus: 'stimfiles/noise1.wav'},
+      {stimulus: 'stimfiles/noise1.wav'},
+      {stimulus: 'stimfiles/AM_4.00_Hz_0.18dep.wav'},
+      ]
+    },
+    {
+      "participant_order": 6,
+      "stimlist": [
+      {stimulus: 'stimfiles/noise1.wav'},
+      {stimulus: 'stimfiles/noise1.wav'},
+      {stimulus: 'stimfiles/noise1.wav'},
+      ]
+    },
+
+];
