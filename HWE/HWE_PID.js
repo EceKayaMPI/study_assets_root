fe077cfa6f649da5210de47dbb072a05152cfc10
EceKayaMPI <61702784+EceKayaMPI@users.noreply.github.com>
2021-04-25 22:44:02 +0200

    HWE al ok

---
+++ HWE/HWE_PID.js
@@ -0,0 +1,91 @@
+
+// single participant stimlist (2-stim trial pairs)
+
+var info_HWE = [
+{
+	"list_order":1,
+	"timevars":[
+		{
+			stim_1:'400_ms.wav',
+			stim_2:'500_ms.wav'
+		},
+		{
+			stim_1:'400_ms.wav',
+			stim_2:'600_ms.wav'
+		},
+		{
+			stim_1:'400_ms.wav',
+			stim_2:'700_ms.wav'
+		},
+		{
+			stim_1:'400_ms.wav',
+			stim_2:'800_ms.wav'
+		},
+		{
+			stim_1:'500_ms.wav',
+			stim_2:'400_ms.wav'
+		},
+		{
+			stim_1:'500_ms.wav',
+			stim_2:'600_ms.wav'
+		},
+		{
+			stim_1:'500_ms.wav',
+			stim_2:'700_ms.wav'
+		},
+		{
+			stim_1:'500_ms.wav',
+			stim_2:'800_ms.wav'
+		},
+		{
+			stim_1:'600_ms.wav',
+			stim_2:'400_ms.wav'
+		},
+		{
+			stim_1:'600_ms.wav',
+			stim_2:'500_ms.wav'
+		},
+		{
+			stim_1:'600_ms.wav',
+			stim_2:'700_ms.wav'
+		},
+		{
+			stim_1:'600_ms.wav',
+			stim_2:'800_ms.wav'
+		},
+		{
+			stim_1:'700_ms.wav',
+			stim_2:'400_ms.wav'
+		},
+		{
+			stim_1:'700_ms.wav',
+			stim_2:'500_ms.wav'
+		},
+		{
+			stim_1:'700_ms.wav',
+			stim_2:'600_ms.wav'
+		},
+		{
+			stim_1:'700_ms.wav',
+			stim_2:'800_ms.wav'
+		},
+		{
+			stim_1:'800_ms.wav',
+			stim_2:'400_ms.wav'
+		},
+		{
+			stim_1:'800_ms.wav',
+			stim_2:'500_ms.wav'
+		},
+		{
+			stim_1:'800_ms.wav',
+			stim_2:'600_ms.wav'
+		},
+		{
+			stim_1:'800_ms.wav',
+			stim_2:'700_ms.wav'
+		}
+	]
+}
+
+];
